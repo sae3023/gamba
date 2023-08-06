1c14a4af9eb0fd40c7ce963cf2bf806d137c7ea0
otto <sae3023@gmail.com>
2023-08-07 01:48:13 +0300

    jsut gotta gamba out how gamba works

--- src/gamba.rs
+++ src/gamba.rs
@@ -1,56 +1,98 @@
-use std::io;
-use std::io::Write;
-use rand::prelude::SliceRandom;
+use std::fmt::format;
+use std::io::{stdout, Write};
+use std::thread;
+use std::time::Duration;
 use rand::RngCore;
 use rand::rngs::ThreadRng;
 
 const FORMAT_SYMBOL: &str = "X";
-const SYMBOLS: [&str; 4] = ["\x1b[0;31m7\x1b[0m ", "🍒", "🍋", "🍇"];
+const RED_SEVEN: &str = "\x1b[0;31m7\x1b[0m ";
+const CHERRY: &str = "🍒";
+const LEMON: &str = "🍋";
+const GRAPE: &str = "🍇";
+const SYMBOLS: [&str; 4] = [RED_SEVEN, CHERRY, LEMON, GRAPE];
 const SLOT_MACHINE_FMT: &str = include_str!("./gamba.txt");
 const SLOT_MACHINE_FMT_LENGTH: usize = SLOT_MACHINE_FMT.len();
+const BUFFER_SIZE: usize = SLOT_MACHINE_FMT_LENGTH + (3 * 14);
 
-struct SlotMachine {
+pub struct SlotMachine {
     first: usize,
     second: usize,
     third: usize,
+    state: Vec<u8>,
 }
 
 impl SlotMachine {
-
     pub fn initialize_at_random(rng: &mut ThreadRng) -> SlotMachine {
-        let first: usize = (rng.next_u32() % SYMBOLS.len()) as usize;
-        let second: usize = (rng.next_u32() % SYMBOLS.len()) as usize;
-        let third: usize = (rng.next_u32() % SYMBOLS.len()) as usize;
+        let first: usize = (rng.next_u32() % SYMBOLS.len() as u32) as usize;
+        let second: usize = (rng.next_u32() % SYMBOLS.len() as u32) as usize;
+        let third: usize = (rng.next_u32() % SYMBOLS.len() as u32) as usize;
+        let state = SlotMachine::make_buffer(first, second, third);
         SlotMachine {
             first,
             second,
-            third
+            third,
+            state,
         }
     }
 
-    pub fn draw(&self) -> String {
-        let mut result = Vec::with_capacity(SLOT_MACHINE_FMT_LENGTH + 5);
-        let indexes = self.as_array();
+    pub fn proceed_state(&mut self, n: usize) {
+        if n >= 3 {
+            self.first = SlotMachine::inc_wrapping(self.first);
+        }
+        if n >= 2 {
+            self.second = SlotMachine::inc_wrapping(self.second);
+        }
+        if n >= 1 {
+            self.third = SlotMachine::inc_wrapping(self.third);
+        }
+        // let mut changed_count: usize = 0;
+        // let commands_to_emit = Vec::with_capacity(10);
+        let mut offset = 0;
+        for x in self.state.as_slice()[119..119 + 65 + 9 * 3].windows(4) {
+            if SlotMachine::contains_emoji(x) {
+                let place_cursor = format!("\x1B[4;{}f", offset + 5);
+                let emoji = SYMBOLS[self.first];
+                stdout().write_all(place_cursor.as_bytes()).unwrap();
+                stdout().flush().unwrap();
+                stdout().write_all(emoji.as_bytes()).unwrap();
+                stdout().flush().unwrap();
+
+                break;
+            } else if x.starts_with("7".as_bytes()) {
+                // println!("amg");
+            }
+            offset += 1;
+        }
+    }
+
+    pub fn draw_to_stdout(&self) {
+        stdout().write(self.state.as_slice()).unwrap();
+        stdout().flush().unwrap();
+    }
+
+    fn make_buffer(first: usize, second: usize, third: usize) -> Vec<u8> {
+        let mut result = Vec::with_capacity(BUFFER_SIZE);
+        let indexes = [first, second, third];
         let mut count = 0;
         for byte in SLOT_MACHINE_FMT.as_bytes() {
             if *byte == b'X' {
                 let symbol = SYMBOLS[indexes[count]].as_bytes();
                 result.extend_from_slice(symbol);
+                count += 1;
             } else {
                 result.push(*byte);
             }
         }
-        String::from_utf8(result).unwrap()
+        result
     }
 
-    pub fn proceed_state(&mut self) {
-        self.first = SlotMachine::inc_wrapping(self.first);
-        self.second = SlotMachine::inc_wrapping(self.second);
-        self.third = SlotMachine::inc_wrapping(self.third);
+    fn contains_emoji(us: &[u8]) -> bool {
+        us.starts_with(CHERRY.as_bytes()) || us.starts_with(GRAPE.as_bytes()) || us.starts_with(LEMON.as_bytes())
     }
 
     fn as_array(&self) -> [usize; 3] {
-        [*&self.first, *&self.second, &self.third]
+        [*&self.first, *&self.second, *&self.third]
     }
 
     fn inc_wrapping(x: usize) -> usize {
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 mod gamba;
 
-use std::io;
+use gamba::SlotMachine;
 use std::io::Write;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
-    gamba::spin_once();
+    let mut rng = rand::thread_rng();
+    let mut machine = SlotMachine::initialize_at_random(&mut rng);
+    machine.draw_to_stdout();
+    thread::sleep(Duration::from_secs(1));
+    machine.proceed_state(3);
+
 }
